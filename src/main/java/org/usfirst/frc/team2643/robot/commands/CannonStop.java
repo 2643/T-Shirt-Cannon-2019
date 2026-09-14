1aaaa3a6d7517d99898215dcfefe41968f2088cf
Cheemifyde <enguyen0709@outlook.com>
2026-09-14 16:18:53 -0700

    Working on updating

--- src/main/java/org/usfirst/frc/team2643/robot/commands/CannonStop.java
+++ src/main/java/org/usfirst/frc/team2643/robot/commands/CannonStop.java
@@ -18,35 +18,35 @@ public class CannonStop extends Command {
   public CannonStop() {
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
-    requires(Robot.cannonPositioner);
+    addRequirements(Robot.cannonPositioner);
   }
 
   // Called just before this Command runs the first time
   @Override
-  protected void initialize() {
+  public void initialize() {
   }
 
   // Called repeatedly when this Command is scheduled to run
   @Override
-  protected void execute() {
+  public void execute() {
     Robot.cannonPositioner.cannonStop();
   }
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
-  protected boolean isFinished() {
+  public boolean isFinished() {
     return false;
   }
 
   // Called once after isFinished returns true
   @Override
-  protected void end() {
+  public void end() {
  }
 
   // Called when another command which requires one or more of the same
   // subsystems is scheduled to run
   @Override
-  protected void interrupted() {
+  public void interrupted() {
     end(); 
   }
 }
